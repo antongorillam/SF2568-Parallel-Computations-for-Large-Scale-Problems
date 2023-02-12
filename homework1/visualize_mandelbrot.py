31434d57b4b1b65ef87789d5fbeae35d61e4a148
Anton <whafu@kth.se>
2023-02-12 12:39:13 +0100

    parallel works?

--- homework1/visualize_mandelbrot.py
+++ homework1/visualize_mandelbrot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
-    data = np.loadtxt("color_seq_c.txt", dtype=int)
+    data = np.loadtxt("color_parallel_c.txt", dtype=int)
     # data = np.loadtxt("color_python.txt", dtype=int)
 
     plt.imshow(data)
